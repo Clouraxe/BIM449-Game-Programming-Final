1f3019d3ef3ad3fe76b46d588dc5f25dc491eced
clouraxe <clouraxe@gmail.com>
2025-12-22 04:02:41 +0300

    Hover mechanincs (On paper only) To add, put an object on Interactable Layer and add Hoverr material

---
+++ Assets/Scripts/PlayerInteractor.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerInteractor : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float interactRange = 3f;
+    [SerializeField] private LayerMask interactLayer;
+    
+    private Camera cam;
+    
+    private Renderer selectedObject;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hit;
+
+        // 1. Raycast detection
+        if (Physics.Raycast(ray, out hit, interactRange, interactLayer))
+        {
+            // 2. Try to get the interface
+            if (hit.collider.TryGetComponent<Renderer>(out Renderer target))
+            {
+                
+
+                // ALWAYS call OnLook (for highlights, crosshair changes, etc.)
+
+                if (selectedObject != target)
+                {
+                    ClearSelection();
+                    target.materials[^1].color = new Color(1f, 1f, 0f, 0.75f);
+                    selectedObject = target;
+                }
+
+                // 3. Check for Click (Left Mouse Button)
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //target.OnClick();
+                }
+            }
+            else ClearSelection();
+        }
+        else ClearSelection();
+    }
+
+    void ClearSelection()
+    {
+        if (selectedObject == null) return;
+        
+        selectedObject.materials[^1].color = new Color(1f, 1f, 0f, 0f);
+        selectedObject = null;
+        
+    }
+}
